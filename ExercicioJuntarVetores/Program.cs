e338fa4c9712429e511cd60b69ea367930e698f6
GBrandao04 <gabrielmartbrandao@gmail.com>
2023-03-07 12:20:26 -0300

    Terceiro Vetor agora é impresso ordenado no fim do programa

--- ExercicioJuntarVetores/Program.cs
+++ ExercicioJuntarVetores/Program.cs
@@ -16,21 +16,45 @@ imprimeVetor(segVetor);
 Console.WriteLine("\nTerceiro vetor é: ");
 imprimeVetor(finalVetor);
 
-double[] inserirVetor(double[] insereA, double[] insereB) 
+ordenaVetor(finalVetor);
+Console.WriteLine("\nTerceiro vetor ordenado é: ");
+imprimeVetor(finalVetor);
+
+void ordenaVetor(double[] vetor)
+{
+    double aux = 0;
+
+    for (int i = 0; i < (vetor.Length - 1); i++)
+    {
+        for (int j = i + 1; j < (vetor.Length); j++)
+        {
+            if (vetor[i] > vetor[j])
+            {
+                aux = vetor[i];
+                vetor[i] = vetor[j];
+                vetor[j] = aux;
+            }
+        }
+    }
+
+  //  return vetor;
+}
+
+double[] inserirVetor(double[] insereA, double[] insereB)
 {
     double[] aux = new double[10];
-    for(int i = 0; i < insereA.Length; i++)
+    for (int i = 0; i < insereA.Length; i++)
         aux[i] = insereA[i];
     for (int i = 0; i < insereB.Length; i++)
-        aux[i+5] = insereB[i];
+        aux[i + 5] = insereB[i];
     return aux;
 }
 
 void imprimeVetor(double[] printVetor)
 {
-    for(int i = 0; i < printVetor.Length; i++)
+    for (int i = 0; i < printVetor.Length; i++)
     {
-        Console.Write(printVetor[i]+ " ");
+        Console.Write(printVetor[i] + " ");
     }
     Console.WriteLine();
 }
@@ -47,7 +71,7 @@ double[] criarVetor(string nome)
             Console.WriteLine("Por favor digite um número válido!\n");
             i--;
         }
-        
+
     }
 
     return Vetor;
